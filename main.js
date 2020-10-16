7009df97ba7df45a9694e945cba8033001c57c6f
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2020-10-16 21:28:03 +0100

    Improvements half module exercise

--- main.js
+++ main.js
@@ -1,29 +1,39 @@
-"use strict"
+"use strict";
 
-const random = document.querySelector("body")
-const button = document.querySelector(".js-button")
-const clue = document.querySelector(".js-clue")
-const attempts = document.querySelector(".js-attempts")
+const random = document.querySelector("body");
+const buttonElement = document.querySelector(".js-btn");
+const inputElement = document.querySelector(".js-input");
+const clueElement = document.querySelector(".js-clue");
+const attemptsElement = document.querySelector(".js-attempts");
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
-  }
-  console.log(getRandomNumber(100));
-
-function getFinalNumber() {
-  const getInputValue = clue.value;
-  clue.innerHTML = getInputValue;
-  const randomNumber = getRandomNumber;
-  if (getInputValue > randomNumber) {
-    clue.value = "Demasiado alto";
-  } else if (getInputValue < randomNumber) {
-    clue.value = "Demasiado bajo";
-  } else if (getInputValue === randomNumber) {
-    clue.value = "Has ganado campeona!!!";
-  } else {
-    clue.value = "El número debe estar entre 1 y 100";
-  }
 }
 
+const randomNumber = getRandomNumber(100);
+console.log(randomNumber);
 
-button.addEventListener("click", getFinalNumber);
+function getFinalNumber() {
+  const userNumber = parseInt(inputElement.value);
+  console.log(userNumber);
+  if (userNumber > 0 && userNumber <= 100) {
+    if (userNumber > randomNumber) {
+      clueElement.innerHTML = "Pista: demasiado alto";
+    } else if (userNumber < randomNumber) {
+      clueElement.innerHTML = "Pista: demasiado bajo";
+    } else if (userNumber === randomNumber) {
+      clueElement.innerHTML = "Has ganado campeona!!!";
+    }
+  } else {
+    clueElement.innerHTML = "Pista: el número debe estar entre 1 y 100";
+  }
+  attemptsElement.innerHTML = getAttemptsNumber();
+}
+
+let attempts = 0;
+function getAttemptsNumber() {
+  attempts++;
+  return `Número de intentos: ${attempts}`;
+}
+
+buttonElement.addEventListener("click", getFinalNumber);
